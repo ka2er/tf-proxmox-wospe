3ee85198ffb2445f792e7f0822f0ab9e0c3d2ab9
ka2er <personseb@wospe.fr>
2022-02-15 21:30:23 +0100

    FIX : run_rtl_433 quote error

--- files/run_rtl_433.sh
+++ files/run_rtl_433.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rtl_433 -C si -F "influx://192.168.10.169:8086/write?db=rtl&p=rtl&u=rtl" -F "mqtt://192.168.10.169:1883,user=rtl,pass=rtl
+rtl_433 -C si -F "influx://192.168.10.169:8086/write?db=rtl&p=rtl&u=rtl" -F "mqtt://192.168.10.169:1883,user=rtl,pass=rtl"
